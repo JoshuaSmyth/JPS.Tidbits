6f2c70201403ab85fb3500ca90805d8037c58b5e
JoshuaSmyth <joshuapaulsmyth@gmail.com>
2017-10-15 19:49:44 +1300

    Added a very simple json writer.

---
+++ SimpleJsonWriter/SimpleJsonWriter/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleJsonWriter
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var writer = new SimpleJsonTextWriter();
+
+            writer.Start();
+                writer.WriteProperty("CPU", "Intel");
+                writer.WriteProperty("PSU", "500W");
+
+                writer.StartArray("Drives");
+                    writer.WriteString("DVD read/write");
+                    writer.WriteString("256GB SSD");
+                    writer.WriteString("1TB HDD");
+                writer.EndArray();
+
+            writer.End();
+
+            var value = writer.ToString();
+        }
+    }
+}
---
+++ SimpleJsonWriter/SimpleJsonWriter/SimpleJsonTextWriter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleJsonWriter
+{
+    /// <summary>
+    /// A simple json writer, provides no string formating
+    /// </summary>
+    public class SimpleJsonTextWriter
+    {
+        private StringBuilder _StringBuilder = new StringBuilder();
+        bool startProperty = false;
+        bool startObject = false;
+        bool startArray = false;
+        public void Start() {
+            _StringBuilder.Append("{");
+            startObject = true;
+        }
+
+        public void WriteProperty(string key, string value) {
+            CheckComma();
+            startProperty = true;
+            _StringBuilder.Append("\"");
+            _StringBuilder.Append(key);
+            _StringBuilder.Append("\":\"");
+            _StringBuilder.Append(value);
+            _StringBuilder.Append("\"");
+        }
+
+        private void CheckComma() {
+            if (startProperty == true) {
+                _StringBuilder.Append(",");
+            }
+        }
+
+        public void End() {
+            _StringBuilder.Append("}");
+        }
+
+        public string ToString() {
+            return _StringBuilder.ToString();
+        }
+
+        public void StartArray(string name) {
+            CheckComma();
+
+            _StringBuilder.Append("\"");
+            _StringBuilder.Append(name);
+            _StringBuilder.Append("\":[");
+
+            startProperty = false;
+            startArray = true;
+        }
+
+        public void EndArray() {
+            startProperty = true;
+            startArray = false;
+            _StringBuilder.Append("]");
+        }
+
+        public void WriteString(string value) {
+            CheckComma();
+            startProperty = true;
+            _StringBuilder.Append("\"");
+            _StringBuilder.Append(value);
+            _StringBuilder.Append("\"");
+        }
+    }
+}
